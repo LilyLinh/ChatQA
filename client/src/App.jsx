cdeffbb87e4bdc53c38565fbf5f38bea058fcf4a
lilylinh <lhacaoth@redhat.com>
2025-08-11 01:50:37 +0100

    add endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/chat", {
+      const res = await fetch("https://stockai-5sf6.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
