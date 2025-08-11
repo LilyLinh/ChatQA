eb26272f709218e18fe25f2acd10e904b3d8d90d
lilylinh <lhacaoth@redhat.com>
2025-08-11 02:14:15 +0100

    add endpoint

--- server/server.js
+++ server/server.js
@@ -276,4 +276,4 @@ app.get("/api/geo/geocode", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server listening on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server listening on ${PORT}`));
